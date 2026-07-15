c9d04f6f48534adf99727c0bd39a4ba38ebb3fa0
sabirovwassas1yaa-cmyk <sabirovwass.as1yaa@gmail.com>
2026-07-15 21:24:18 +0600

    Finish homework 3

--- Love/urls.py
+++ Love/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.nurai.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index-page'),
 ]
 
-# Подключаем media файлы в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/nurai/views.py
+++ apps/nurai/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+
+def index(request):
+    students = Student.objects.all()
+    return render(request,"index.html",locals())
